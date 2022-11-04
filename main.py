efae44886e8bcb8ba572a0fa08fd9d658323ea2d
Verónica QD <karla.quiros.delgado@est.una.ac.cr>
2022-11-03 18:11:03 -0600

    merge

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Juego(QMainWindow):
         self.matriz_len:int = 3
         self.gano = False
         self.matriz = [[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
+        self.modo:int = self.dificultad.currentIndex()
 
         self.button_00.clicked.connect(lambda:self.botones_jugador("00"))
         self.button_01.clicked.connect(lambda:self.botones_jugador("01"))
@@ -25,7 +26,10 @@ class Juego(QMainWindow):
         self.button_20.clicked.connect(lambda:self.botones_jugador("20"))
         self.button_21.clicked.connect(lambda:self.botones_jugador("21"))
         self.button_22.clicked.connect(lambda:self.botones_jugador("22"))
+        self.dificultad.currentIndexChanged.connect(self.set_modo)
 
+    def set_modo(self):
+        self.modo = self.dificultad.currentIndex()
     def set_turno(self, turno:int):
        self.turno = turno
        
@@ -81,67 +85,90 @@ class Juego(QMainWindow):
         return ganador
 
     def botones_jugador(self, valor:str):
-        if self.gana() != "-1":
-            if (self.gana() == "x"):
-                self.mensaje.setText("El jugador ganó")
-            else:
-                self.mensaje.setText("El CPU ganó")
-            return
+        if self.modo == 0:
+            if self.gana() != "-1":
+                if (self.gana() == "x"):
+                    self.mensaje.setText("El jugador ganó")
 
-        if (valor == "AI"): pass
-        elif self.matriz[int(valor[0])][int(valor[1])] == "x" or self.matriz[int(valor[0])][int(valor[1])] == "o": return
+                else:
+                    self.mensaje.setText("El CPU ganó")
+                return
 
-        turno_icon = QtGui.QIcon("resource/x.png")
-        turno_letra = "x"
-        if self.turno % 2 == 0:
-            turno_icon = QtGui.QIcon("resource/o.png")
-            turno_letra = "o"
-            vacio:bool = False # Easy level starts, fix AI turn, delete this to PVP.
-            i:int = -1
-            j:int = -1
-            while not vacio:
-                i = random.randint(0, 2)
-                j = random.randint(0, 2)
-                if self.matriz[i][j] == -1:
-                    vacio = True
-            valor = str(i) + str(j)
-            print(f'Estoy jugando en una pos. random: {i},{j}')
+            if (valor == "AI"): pass
+            elif self.matriz[int(valor[0])][int(valor[1])] == "x" or self.matriz[int(valor[0])][int(valor[1])] == "o": return
 
-        if (valor == "00"): self.button_00.setIcon(turno_icon)
-        if (valor == "01"): self.button_01.setIcon(turno_icon)
-        if (valor == "02"): self.button_02.setIcon(turno_icon)
-        if (valor == "10"): self.button_10.setIcon(turno_icon)
-        if (valor == "11"): self.button_11.setIcon(turno_icon)
-        if (valor == "12"): self.button_12.setIcon(turno_icon)
-        if (valor == "20"): self.button_20.setIcon(turno_icon)
-        if (valor == "21"): self.button_21.setIcon(turno_icon)
-        if (valor == "22"): self.button_22.setIcon(turno_icon)
+            turno_icon = QtGui.QIcon("resource/x.png")
+            turno_letra = "x"
+            if self.turno % 2 == 0:
+                turno_icon = QtGui.QIcon("resource/o.png")
+                turno_letra = "o"
+                vacio:bool = False # Easy level starts, fix AI turn, delete this to PVP.
+                i:int = -1
+                j:int = -1
+                while not vacio:
+                    i = random.randint(0, 2)
+                    j = random.randint(0, 2)
+                    if self.matriz[i][j] == -1:
+                        vacio = True
+                valor = str(i) + str(j)
+                print(f'Estoy jugando en una pos. random: {i},{j}')
 
-        if self.turno == 9:
-            for i in range(self.matriz_len):
-                for j in range(self.matriz_len):
-                    if self.matriz[i][j] == -1: self.matriz[i][j] = "x"
+            if (valor == "00"): self.button_00.setIcon(turno_icon)
+            if (valor == "01"): self.button_01.setIcon(turno_icon)
+            if (valor == "02"): self.button_02.setIcon(turno_icon)
+            if (valor == "10"): self.button_10.setIcon(turno_icon)
+            if (valor == "11"): self.button_11.setIcon(turno_icon)
+            if (valor == "12"): self.button_12.setIcon(turno_icon)
+            if (valor == "20"): self.button_20.setIcon(turno_icon)
+            if (valor == "21"): self.button_21.setIcon(turno_icon)
+            if (valor == "22"): self.button_22.setIcon(turno_icon)
+
+            if self.turno == 9:
+                for i in range(self.matriz_len):
+                    for j in range(self.matriz_len):
+                        if self.matriz[i][j] == -1: self.matriz[i][j] = "x"
+                self.print_matriz()
+                if (self.gana() == "x"):
+                    self.mensaje.setText("El jugador ganó")
+
+                elif (self.gana() == "o"):
+                    self.mensaje.setText("El CPU ganó")
+
+                else :self.mensaje.setText("Empate")
+                return
+            self.matriz[int(valor[0])][int(valor[1])] = turno_letra
             self.print_matriz()
-            if (self.gana() == "x"):
-                self.mensaje.setText("El jugador ganó")
-            elif (self.gana() == "o"):
-                self.mensaje.setText("El CPU ganó")
-            else :self.mensaje.setText("Empate")
-            return
-        self.matriz[int(valor[0])][int(valor[1])] = turno_letra
-        self.print_matriz()
-        self.turno += 1
-        if self.turno % 2 == 0: self.botones_jugador("AI")
-        if self.gana() != "-1":
-            if(self.gana() == "x"):
-                self.mensaje.setText("El jugador ganó")
-            else:
-                self.mensaje.setText("El CPU ganó")
-            return
+            self.turno += 1
+            if self.turno % 2 == 0: self.botones_jugador("AI")
+            if self.gana() != "-1":
+                if(self.gana() == "x"):
+                    self.mensaje.setText("El jugador ganó")
 
+                else:
+                    self.mensaje.setText("El CPU ganó")
 
+                return
+        if self.modo == 1:
+            #modo intermedio
+            return;
+        if self.modo == 2:
+            # Modo dificil
+            return;
 
-
+    def reset(self):
+        self.matriz = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+        self.turno = 1
+        self.gano = False
+        self.button_00.setIcon(QtGui.QIcon())
+        self.button_01.setIcon(QtGui.QIcon())
+        self.button_02.setIcon(QtGui.QIcon())
+        self.button_10.setIcon(QtGui.QIcon())
+        self.button_11.setIcon(QtGui.QIcon())
+        self.button_12.setIcon(QtGui.QIcon())
+        self.button_20.setIcon(QtGui.QIcon())
+        self.button_21.setIcon(QtGui.QIcon())
+        self.button_22.setIcon(QtGui.QIcon())
+        self.mensaje.setText("")
 
 if __name__ == "__main__":
 
