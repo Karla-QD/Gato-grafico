e0a903dba8a122a7277c57d093e752c5bda8d7c2
alisteller <97266093+alisteller@users.noreply.github.com>
2022-11-14 15:44:11 -0600

    Clases

--- unitTesting/pruebas_unidad.py
+++ unitTesting/pruebas_unidad.py
@@ -6,19 +6,24 @@ from PyQt5 import uic, QtGui
 
 
 class TestCaseJvsJ(unittest.TestCase):
+
     def __int__(self):
         self.jugador = Jugador_Jugador.Jugador_Jugador()
+        self.juego = Juego.Juego()
+        self.matriz = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+
+    #def test_jvsC(self):
+     #   result = "x"
+      #  self.assertEqual(result, "x")
+
     def test_jvsj(self):
         self.jugador = Jugador_Jugador.Jugador_Jugador()
-        self.matriz = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
-        self.matriz[0][0] = "x"
-        self.matriz[0][1] = "x"
-        self.matriz[0][2] = "x"
-        self.jugador.matriz[0][0] = self.matriz[0][0]
-        self.jugador.matriz[0][1] = self.matriz[0][1]
-        self.jugador.matriz[0][2] = self.matriz[0][2]
-        self.assertEqual(self.jugador.gana(), True)
-
+        self.matriz = []
+        self.jugador.matriz[0][0] = "x"
+        self.jugador.matriz[0][1] = "x"
+        self.jugador.matriz[0][2] = "x"
+        result = self.jugador.gana()
+        self.assertEqual(result, "x")
 
 
 if __name__ == '__main__':
